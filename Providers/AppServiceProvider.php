2984e64f170ec8ac02e0e485c06ed95bfa9f37df
Levi's Luong <levi.dev.xxx@gmail.com>
2023-01-27 00:43:22 +0700

    update

---
+++ Providers/AppServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: levi
+ * Date: 27/01/2023
+ * Time: 00:40
+ */
+
+namespace OctopusOsc\Framework\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+//        die();
+    }
+
+    public function register()
+    {
+//        die();
+    }
+}
